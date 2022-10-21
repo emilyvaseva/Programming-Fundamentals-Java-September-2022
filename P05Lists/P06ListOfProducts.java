48fc275a1a09750f7e241a3f4d9b086e7900ddaa
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2022-10-21 18:38:09 +0300

    Add tasks for exercise

---
+++ P05Lists/P06ListOfProducts.java
@@ -0,0 +1,26 @@
+package P05Lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class P06ListOfProducts {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<String> productList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String products = scanner.nextLine();
+
+            productList.add(products);
+        }
+        Collections.sort(productList);
+
+        for (int i = 0; i < productList.size(); i++) {
+            System.out.println(i + 1 + "." + productList.get(i));
+        }
+    }
+}
+
