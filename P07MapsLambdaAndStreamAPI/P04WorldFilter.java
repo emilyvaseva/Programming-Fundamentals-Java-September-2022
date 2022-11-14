2d508cca52882f213cd86cc6d3e809127e6d2483
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2022-11-14 15:36:55 +0200

    Add tasks

---
+++ P07MapsLambdaAndStreamAPI/P04WorldFilter.java
@@ -0,0 +1,16 @@
+package P07MapsLambdaAndStreamAPI;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P04WorldFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] wordsArr= Arrays.stream(scanner.nextLine().split(" "))
+                .filter(w -> w.length()%2==0)
+                .toArray(String[]::new);
+
+        System.out.println(String.join(System.lineSeparator(),wordsArr));
+    }
+}
